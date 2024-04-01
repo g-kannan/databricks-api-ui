e09865dcd084efbe987956047b537fea3a06b139
Kannan <22211670+g-kannan@users.noreply.github.com>
2024-04-01 14:25:10 +0530

    update Workspace_secrets.py

--- pages/Workspace_secrets.py
+++ pages/Workspace_secrets.py
@@ -5,10 +5,17 @@ from databricks.sdk.service import compute
 st.session_state['url'] = st.text_input("Enter Databricks URL")
 st.session_state['token'] = st.text_input("Enter token")
 
+
 if st.button("List Scopes"):
     w = WorkspaceClient(host=st.session_state['url'], token=st.session_state['token'])
     scopes = w.secrets.list_scopes()
     st.dataframe(scopes)
+    
+with st.expander("Create scope"):
+    v_scope_name = st.text_input("scope name")
+    if st.button("Create"):
+        w = WorkspaceClient(host=st.session_state['url'], token=st.session_state['token'])
+        w.secrets.create_scope(scope=v_scope_name)
 
 
 
